a3d81efd8daa9ddc25c4c15242738a709a5d835b
MELISSA PIZARRO <mpizarro03@gmail.com>
2019-12-30 10:21:01 -0700

    wire up action in app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { getSeriesVideoRequest } from "./actions/index";
 
 class App extends Component {
+  async componentDidMount() {
+    this.props.getSeriesVideoRequest(122881);
+  }
   render() {
     return (
       <div className="app">
         <header className="app__header">
           <h1 className="app__main-heading">{`Hello!  This is the <App /> component!  Please render your UI in .app__body below`}</h1>
         </header>
-        <div className="app__body">
-          {/* TODO: render UI here */}
-        </div>
+        <div className="app__body">{/* TODO: render UI here */}</div>
       </div>
     );
   }
 }
+const mapStateToProps = state => {
+  return {
+    seriesData: state.seriesData
+  };
+};
 
-export default App;
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ getSeriesVideoRequest }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
