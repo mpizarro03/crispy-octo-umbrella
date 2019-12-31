64654b4ce6c879d03fffb35c998a358245cfe697
MELISSA PIZARRO <mpizarro03@gmail.com>
2019-12-31 11:08:00 -0700

    import components and pass props to handle series loading state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import React, { Component } from "react";
-import "./App.css";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import "./App.css";
 import { getSeriesVideoRequest } from "./actions/index";
+//components
+import SearchBar from "./components/SearchBar";
+import InfoDisplayCard from "./components/Card/Card";
+import Loading from "./components/Loading/Loading";
 
 class App extends Component {
-  async componentDidMount() {
-    this.props.getSeriesVideoRequest(122881);
-  }
   render() {
+    const { seriesData } = this.props;
+    const seriesProcessing = seriesData && seriesData.isLoading === true;
+    const seriesLoaded =
+      seriesData &&
+      seriesData.isLoading === false &&
+      seriesData.seriesHeroArt &&
+      seriesData.seriesTitle;
+    const seriesError =
+      seriesData &&
+      seriesData.error &&
+      seriesData.isLoading === false &&
+      seriesData.error.isAxiosError;
     return (
-      <div className="app">
-        <header className="app__header">
-          <h1 className="app__main-heading">{`Hello!  This is the <App /> component!  Please render your UI in .app__body below`}</h1>
+      <div className="app container">
+        <header className="app__header row">
+          <h1>{`Hello!  This is the <App /> component!  Please render your UI in .app__body below`}</h1>
         </header>
-        <div className="app__body">{/* TODO: render UI here */}</div>
+
+        <div className="app__body row">
+          <SearchBar />
+        </div>
+        <div className="app__body row">
+          <Loading
+            data={seriesData}
+            processing={seriesProcessing}
+            loaded={seriesLoaded}
+            error={seriesError}
+          />
+        </div>
+        <div>{seriesLoaded && <InfoDisplayCard series={seriesData} />}</div>
       </div>
     );
   }
