dd82ed62b5aad09921ab188873e69a4e8f8ea4a4
MELISSA PIZARRO <mpizarro03@gmail.com>
2019-12-31 11:02:02 -0700

    create loading component to render loading or error message

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Loading.css";
+
+function Loading({ processing, error }) {
+  return (
+    <div>
+      {processing && <div style={{ color: "red " }}>Not Found</div>}
+      {error && <div style={{ color: "red " }}>Not Found</div>}
+    </div>
+  );
+}
+
+Loading.defaultProps = {
+  processing: false,
+  loaded: false,
+  error: false
+};
+
+Loading.propTypes = {
+  processing: PropTypes.bool,
+  loaded: PropTypes.bool,
+  error: PropTypes.bool
+};
+
+export default React.memo(Loading);
